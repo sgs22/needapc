8d2fb7472d1f3753aeee542eaa569f7fb7e6db9c
sgs22 <samuel_smith@outlook.com>
2021-02-10 17:53:49 +0000

    rm previous model comment

--- products/models.py
+++ products/models.py
@@ -7,23 +7,10 @@ from django.db import models
     sorts them by date uploaded
 
 """
-# class ProductDetail(models.Model):
-#     name = models.CharField(max_length=120, blank=False) 
-#     price = models.DecimalField(max_digits=6, decimal_places=2)
-#     image = models.ImageField(upload_to='products/%Y/%m/%d/', max_length=255, null=True, blank=True)
-#     brand = models.CharField(max_length=120, blank=True)
-#     modelnumber = models.CharField(max_length=120, blank=True)
-#     size = models.CharField(max_length=120, blank=True)
-#     weight = models.CharField(max_length=120, blank=True)
-#     # url = models.CharField(max_length=120, blank=True)
-#     updated = models.DateTimeField(auto_now=True) # sets when saved
-#     timestamp = models.DateTimeField(auto_now_add=True) # sets when added
-
-
 class ProductDetail(models.Model):
     name = models.CharField(max_length=120, blank=False) 
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='products/%Y/%m/%d/', max_length=255, null=True, blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', max_length=255, null=True, blank=True) #this might change to img urls...
     url = models.CharField(max_length=120, blank=True)
     description_overview = models.CharField(max_length=120, blank=True)
     brand = models.CharField(max_length=120, blank=True)
