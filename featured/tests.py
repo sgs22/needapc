6cc1ce012ad0483c1e1d2728fea62433785d8e6d
sgs22 <samuel_smith@outlook.com>
2021-03-30 17:17:08 +0100

    add tests to featured

--- featured/tests.py
+++ featured/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import FeaturedPost
 
-# Create your tests here.
+class FeaturedTestCase(TestCase):
+    def setUp(self):
+        FeaturedPost.object.create(title="testFeatured",price="99", slug="testfeatured")
+
+    def test_featured_status(self):
+        """Make sure status isnt published"""
+        featuredPost = FeaturedPost.objects.get(title="testFeatured")
+        self.assertEqual(featuredPost.status(), 0)
