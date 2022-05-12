bee9b6cb91f1ff12c42399e3665932bde7abee4a
sgs22 <samuel_smith@outlook.com>
2022-05-12 23:25:22 +0100

    add logic to preview questions and choices within single view

--- quizApp/views.py
+++ quizApp/views.py
@@ -19,26 +19,27 @@ class QuizDetail(generic.DetailView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        print(context)
-        print(context.get('object').slug)
-        print(context.get('object'))
+        # print(context)
+        # print(context.get('object').slug)
+        # print(context.get('object'))
         # print(Question.objects.first())
         quiz_id = context.get('object').id
         # Add in a QuerySet of all the questions
-        context['question_list'] = Question.objects.select_related().filter(quiz = quiz_id)
+        context['question_list'] = Question.objects.select_related().filter(quiz=quiz_id)
+        context['choice_list'] = Choice.objects.all()
         question_id = context.get('object').id
-        question_choices = {}
         for question in context['question_list']:
-            question_object = Question.objects.get(id=question.id)
-            question_choices[question_id] = question_object.choice_set.all()
-        print(question_choices)
-        #print(context['question_list'].id)
-        # question = context['question_list'].values.id
-        context['choice_list'] = Choice.objects.select_related()
+            print(question.question_choices.all().values())
+            # context['question_choices'] = question.choice_set.all()
 
+        # print(context['question_list'].id)
+        # question = context['question_list'].values.id
+        # context['choice_list'] = Choice.objects.select_related()
+        # print(context['choice_list'])
 
         # reverse lookup - question.choice_set.all() gets all related choices to that question
-        context['choice_list_reverselookup'] = question_choices.values()
+        # context['choice_list_reverselookup'] = question_choices.values()
+
         return context
 
     #def get_children(self, **kwargs):
