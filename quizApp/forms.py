c44704e909f69c942bd29af25bf0d5d7045fea4a
sgs22 <samuel_smith@outlook.com>
2022-05-15 21:19:02 +0100

    add modelform for quizapp

--- quizApp/forms.py
+++ quizApp/forms.py
@@ -1,22 +1,22 @@
 from django import forms
+from .models import QuizAnswer
 
-class AnswerForm(forms.Form):
-    answer = forms.CharField(max_length=100)
-   
 
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = QuizAnswer
+        fields = '__all__'
 
     # you have your questions written out on the page
     #################################################
     # for each question in question model where quix = x {
     #   display question.name = show
     #       
-    #       for each choice in thhat question {
+    #       for each choice in that question {
     #           display choice.name = show
     #   }
 
     # }
-
-
 
     # now you can display all choices for each question on a page
     # 
