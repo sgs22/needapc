4e1c982e7f83f93ae56f2bd742e21f4d0cbe5437
sgs22 <samuel_smith@outlook.com>
2022-07-11 18:25:47 +0100

    add form_valid - this will be changed

--- quizApp/views.py
+++ quizApp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.views.generic import DetailView
 
-from .models import Quiz, Question, Choice, QuizAnswer
+from .models import Quiz, Question, Choice, QuizAnswer, User
 
 from .forms import AnswerForm
 
@@ -17,23 +19,25 @@ class QuizDetail(generic.DetailView):
     model = Quiz
     template_name = 'quizApp/quiz_detail.html'
 
-
+    def form_valid(self, form):
+        form = form.save(commit=False)
+        form.user = User.objects.get(user=self.request.user)
+        form.save()
+        print(form.user)
+        return redirect('quizapp/')
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         # print(context)
-        # print(context.get('object').slug)
-        # print(context.get('object'))
-        # print(Question.objects.first())
         quiz_id = context.get('object').id
         # Add in a QuerySet of all the questions
         context['question_list'] = Question.objects.select_related().filter(quiz=quiz_id)
         context['choice_list'] = Choice.objects.all()
         question_id = context.get('object').id
-        #for question in context['question_list']:
+        # for question in context['question_list']:
         #    print(question.question_choices.all().values())
-            # context['question_choices'] = question.choice_set.all()
+        # context['question_choices'] = question.choice_set.all()
 
         # print(context['question_list'].id)
         # question = context['question_list'].values.id
@@ -41,7 +45,6 @@ class QuizDetail(generic.DetailView):
         # print(context['choice_list'])
 
         # reverse lookup - question.choice_set.all() gets all related choices to that question
-        # context['choice_list_reverselookup'] = question_choices.values()
         form = AnswerForm()
         context['form'] = form
         return context
