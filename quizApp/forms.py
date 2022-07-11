fb4014bb5085538c3c80ae936256576e66850fdb
sgs22 <samuel_smith@outlook.com>
2022-07-11 18:24:28 +0100

    add get related fields to models

--- quizApp/forms.py
+++ quizApp/forms.py
@@ -7,14 +7,13 @@ class AnswerForm(forms.ModelForm):
 
     class Meta:
         model = QuizAnswer
-        fields = '__all__'
+        exclude = ["user"]
 
     def __init__(self, *args, **kwargs):
         super(AnswerForm, self).__init__(*args, **kwargs)
         self.fields['answer_choice'].queryset = Choice.objects.filter(id=1)
-        self.fields['user'].queryset = User.objects.filter(id=1)
 
 
 
-    # init method to filter out choices for each modelform for each question request.user
+  
 
