ce0a6776ebd4f10df88f33579d3bbab6311a6aac
Samuel Smith <samuel_smith@outlook.com>
2022-07-19 14:26:03 +0100

    add get functions for related fields

--- quizApp/forms.py
+++ quizApp/forms.py
@@ -1,6 +1,7 @@
+from tkinter import Widget
 from django import forms
 from django.contrib.auth.models import User
-from .models import QuizAnswer, Choice
+from .models import QuizAnswer, Choice, Question
 
 
 class AnswerForm(forms.ModelForm):
@@ -8,11 +9,15 @@ class AnswerForm(forms.ModelForm):
     class Meta:
         model = QuizAnswer
         exclude = ["user"]
+        widgets = {
+            'answer_choice': forms.RadioSelect
+        }
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, question, *args, **kwargs):
         super(AnswerForm, self).__init__(*args, **kwargs)
-        self.fields['answer_choice'].queryset = Choice.objects.filter(id=1)
-
+        self.fields['answer_choice'] = forms.ChoiceField(
+            choices=[(o.id, str(o)) for o in Choice.objects.filter(id=question)]
+        )
 
 
   
