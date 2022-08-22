e39400198d3e454cffe78c8b19c3162794b5b187
Samuel Smith <Samuel_Smith@outlook.com>
2022-08-22 20:19:32 +0200

    add config for quizapp overview

--- quizApp/urls.py
+++ quizApp/urls.py
@@ -8,6 +8,7 @@ app_name = 'quizapp'
 urlpatterns = [
     path('quizapp/<slug:slug>/<int:pk>/', views.question_detail, name='question_detail'),
     # path('quizapp/questions/', views.QuestionList.as_view(), name='question_list'),
+    path('quizapp/<slug:slug>/overview', views.quiz_overview, name='quiz_overview'),
     path('quizapp/<slug:slug>/', views.quiz_detail, name='quiz_detail'),
     path('quizapp/', views.QuizList.as_view(), name='quiz_list')
 ]
